b1bf02c0374c713830ef12096c4c9db65c11e408
John Doe <m6urns@gmail.com>
2024-03-05 15:42:59 -0700

    confusing naming conventions guys

--- effector.php
+++ effector.php
@@ -326,7 +326,7 @@
             formData.append("species", $('select#puccinia').val());
                 
             var request = new XMLHttpRequest();
-            request.open("POST", "http://127.0.1.1:80/trustdb/queryEffProtein.php",true);
+            request.open("POST", "queryEffProtein.php", true);
             request.onload = function () {
                 if (request.readyState === request.DONE) {
                     if (request.status === 200) {
